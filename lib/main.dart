2c4963bf7f96ddcc39d2e0cf7668afa2b2fbd29e
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2023-12-25 15:11:23 +0000

    - some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,9 +8,34 @@ import 'common/navigation/auto_router/app_router.dart';
 
 void main() {
   configureDependencies();
+  registerErrorHandlers();
+
   runApp(
     MyApp(),
   );
+}
+
+void registerErrorHandlers() {
+  // * Show some error UI if any uncaught exception happens
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint(details.toString());
+  };
+  // * Handle errors from the underlying platform/OS
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+    debugPrint(error.toString());
+    return true;
+  };
+  // * Show some error UI when any widget in the app fails to build
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('An error occurred'),
+      ),
+      body: Center(child: Text(details.toString())),
+    );
+  };
 }
 
 class MyApp extends StatelessWidget {
